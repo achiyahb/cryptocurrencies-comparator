536c90c80aa503241c2cc9bd85599b30133073ed
achiyahb <achiyahb@gmail.com>
2022-04-14 15:31:11 +0300

    set up prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,19 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '^import\\s.+\\sfrom\\s.+;$'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    semi: [2, 'always'],
+    'max-len': ['error', { code: 120, tabWidth: 2, ignorePattern: '^import\\s.+\\sfrom\\s.+;$' }],
   },
 };
