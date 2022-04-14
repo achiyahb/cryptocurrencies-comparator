827aa5547f584f51c8cae3ce011b4f36c9c208b4
achiyahb <achiyahb@gmail.com>
2022-04-14 15:46:06 +0300

    add validation for get price inputs

--- src/prices/prices.controller.ts
+++ src/prices/prices.controller.ts
@@ -1,11 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, ParseArrayPipe, Query } from '@nestjs/common';
 import { PricesService } from './prices.service';
+import { availableFiatCurrencies } from '../currencies/currencies.constants';
 
 @Controller('prices')
 export class PricesController {
-  constructor(private readonly pricesService: PricesService) {}
+	constructor(private readonly pricesService: PricesService) {}
 
-  async getPrices() {
-    return await this.pricesService.getPrices();
-  }
+	@Get()
+	async getPrices(
+		@Query('fiat', ParseArrayPipe) fiatCurrencies: string[] = ['USD'],
+		@Query('crypto', ParseArrayPipe) cryptoCurrencies: string[]
+	) {
+		this.validateAvailableFiat(fiatCurrencies);
+		this.validateCryptoAllowedLength(cryptoCurrencies);
+		return await this.pricesService.getPrices(fiatCurrencies, cryptoCurrencies);
+	}
+
+	private validateAvailableFiat(currencies: string[]) {
+		const notValidFiats = currencies.filter((currency) => !availableFiatCurrencies.includes(currency));
+		if (notValidFiats.length) {
+			throw new HttpException(
+				`${notValidFiats.join(', ')} ${
+					notValidFiats.length === 1 ? 'is' : 'are'
+				} not part of the available fiat currencies`,
+				HttpStatus.BAD_REQUEST
+			);
+		}
+	}
+
+	private validateCryptoAllowedLength(cryptoCurrencies: string[]) {
+		if (cryptoCurrencies.length > 30) {
+			throw new HttpException('cryptocurrencies can not be more than 30', HttpStatus.BAD_REQUEST);
+		}
+	}
 }
